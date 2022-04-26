c30cfd87c3e254d62e57e07707dd798c9df3ba03
Percon12 <fcorreiaandrade@gmail.com>
2022-04-26 13:01:01 -0300

    Alterando Interface De "Bate-Papo" para "Chat"

--- client.c
+++ client.c
@@ -17,202 +17,232 @@
 #define MAXDATASIZE 500
 #define MAXALLOC 500
 
-int main(){
+int main()
+{
 	char userName[50];
 	char messageChat[250];
 	char bufferKey, *bufferSend, *bufferRcv, *_bufferRcv;
-	int flow,opt,opt2,numBytes,isChatting;
-	
+	int flow, opt, opt2, numBytes, isChatting;
+
 	struct userList _users;
 	struct messageList _messages;
-	
-	int localSocket,conection;
-	struct sockaddr_in remoto;	
-	
-	//Alocação de memória
-	bufferRcv = (char *) calloc (MAXALLOC, sizeof(char));
-	_bufferRcv = (char *) calloc (MAXALLOC, sizeof(char));
-	bufferSend = (char *) calloc (MAXALLOC, sizeof(char));
+
+	int localSocket, conection;
+	struct sockaddr_in remoto;
+
+	// Alocação de memória
+	bufferRcv = (char *)calloc(MAXALLOC, sizeof(char));
+	_bufferRcv = (char *)calloc(MAXALLOC, sizeof(char));
+	bufferSend = (char *)calloc(MAXALLOC, sizeof(char));
 
 	localSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(localSocket == -1)
+	if (localSocket == -1)
 		errorSocket();
-	
-	//Configuração do socket para a conexão
-	//Mais detalhes consulte as manpages
-	remoto.sin_family = AF_INET; 
-	remoto.sin_port = htons(31337);  	
-	remoto.sin_addr.s_addr = inet_addr("127.0.0.1");
-	bzero(&(remoto.sin_zero), 8); //Trocar por memser
 
-	//Função de conexão ao host remoto
+	// Configuração do socket para a conexão
+	// Mais detalhes consulte as manpages
+	remoto.sin_family = AF_INET;
+	remoto.sin_port = htons(31337);
+	remoto.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bzero(&(remoto.sin_zero), 8); // Trocar por memser
+
+	// Função de conexão ao host remoto
 	conection = connect(localSocket, (struct sockaddr *)&remoto, sizeof(remoto));
-	if(conection == -1)
-		errorConnection();	
-	
+	if (conection == -1)
+		errorConnection();
+
 	printAnimation();
-	
-		do{
-		printHeader("Bate-papo");
+
+	do
+	{
+		printHeader("Chat");
 		printMenuOptions();
 		scanf("%d", &opt);
-			
-			//Verificação do primeiro menu
-			switch(opt){
-				case 1:{	
-					flow = 0;				
-					while(!flow){	
-						printHeader("Fase inicial");
-						printf("Escolha seu apelido : ");
-						scanf(" %s", userName);	
 
-						bufferSend = generateMessage(userName, USERNAME,0);
-						send(localSocket, bufferSend, MAXDATASIZE, 0);	
+		// Verificação do primeiro menu
+		switch (opt)
+		{
+		case 1:
+		{
+			flow = 0;
+			while (!flow)
+			{
+				printHeader("Fase inicial");
+				printf("Escolha seu apelido : ");
+				scanf(" %s", userName);
+
+				bufferSend = generateMessage(userName, USERNAME, 0);
+				send(localSocket, bufferSend, MAXDATASIZE, 0);
+
+				numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
+				bufferRcv[numBytes] = '\0';
+				strcpy(_bufferRcv, bufferRcv);
+
+				if (checkKindMessage(_bufferRcv, 0) == CONNECTED)
+				{
+					printAlert("Conexão estabelecida");
+					sleep(1);
+					flow = 1;
+				}
+				else if (checkKindMessage(_bufferRcv, 0) == INVALIDUSERNAME)
+				{
+					printAlert("Nome inválido");
+					sleep(1);
+					flow = 0;
+				}
+			}
+
+			do
+			{
+				printHeader("Chat");
+				printSecondMenuOptions();
+				scanf("%d", &opt2);
+
+				// Verificação do segundo menu
+				switch (opt2)
+				{
+				// Solicita ao servidor a lista de usuários onlines
+				case 1:
+				{
+					bufferSend = generateMessage("", ACTIVEUSERS, 0);
+					send(localSocket, bufferSend, MAXDATASIZE, 0);
+
+					int i = 0;
+					_users.size = i;
+
+					numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
+					bufferRcv[numBytes] = '\0';
+					strcpy(_bufferRcv, bufferRcv);
+
+					while (checkKindMessage(_bufferRcv, 0) != FINISHED)
+					{
+						_users.size = i + 1;
+						strcpy(_users.name[i], checkMessage(bufferRcv));
+						i++;
 
 						numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
 						bufferRcv[numBytes] = '\0';
 						strcpy(_bufferRcv, bufferRcv);
 
-						if(checkKindMessage(_bufferRcv,0) == CONNECTED){
-							printAlert("Conexão estabelecida");
-							sleep(1);
-							flow = 1;
-						}else if(checkKindMessage(_bufferRcv,0) == INVALIDUSERNAME){
-							printAlert("Nome inválido");
-							sleep(1);
-							flow = 0;
+						if (checkKindMessage(_bufferRcv, 0) == FINISHED)
+						{
+							printUserList(_users);
+							waitKey();
+						}
+						strcpy(_bufferRcv, bufferRcv);
+					}
+
+					break;
+					// Entra na sala de conversa
+				}
+				case 2:
+				{
+					printHeader("Sala de conversa");
+
+					isChatting = 1;
+					while (isChatting)
+					{
+						fflush(stdin);
+						while (!kbhit())
+						{
+
+							bufferSend = generateMessage("", UPDATECHAT, 0);
+							send(localSocket, bufferSend, MAXDATASIZE, 0);
+
+							int i = 0;
+							_messages.size = i;
+
+							numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
+							bufferRcv[numBytes] = '\0';
+							strcpy(_bufferRcv, bufferRcv);
+
+							while (checkKindMessage(_bufferRcv, 0) != FINISHED)
+							{
+								_messages.size = i + 1;
+								strcpy(_messages.content[i], checkMessage(bufferRcv));
+								i++;
+
+								numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
+								bufferRcv[numBytes] = '\0';
+								strcpy(_bufferRcv, bufferRcv);
+
+								if (checkKindMessage(_bufferRcv, 0) == FINISHED)
+								{
+									printMessageList(_messages);
+								}
+
+								strcpy(_bufferRcv, bufferRcv);
+							}
+							usleep(500000);
+						}
+
+						bufferKey = getchar();
+						if (bufferKey == '>')
+						{
+							printf(" [%s] - Voce: ", getHourMinutes());
+							scanf(" %[^\n]s", messageChat);
+							printf("\n");
+
+							bufferSend = generateMessage(messageChat, MESSAGECHAT, 0);
+							send(localSocket, bufferSend, MAXDATASIZE, 0);
+						}
+						else if (bufferKey == 'v')
+						{
+							isChatting = 0;
+						}
+						else if (bufferKey == 'l')
+						{
+							printHeader("Sala de conversa");
+						}
+						else
+						{
+							fflush(stdin);
 						}
 					}
-	
-						do{
-							printHeader("Bate-papo");
-							printSecondMenuOptions();
-							scanf("%d", &opt2);
-							
-							//Verificação do segundo menu
-							switch(opt2){
-								//Solicita ao servidor a lista de usuários onlines
-								case 1:{
-									bufferSend = generateMessage("", ACTIVEUSERS,0);
-									send(localSocket, bufferSend, MAXDATASIZE, 0);	
 
-									int i = 0;
-									_users.size = i;
-
-									numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
-									bufferRcv[numBytes] = '\0';
-									strcpy(_bufferRcv, bufferRcv);
-
-									while(checkKindMessage(_bufferRcv,0) != FINISHED){
-										_users.size = i+1;
-										strcpy(_users.name[i], checkMessage(bufferRcv));
-										i++;
-
-										numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
-										bufferRcv[numBytes] = '\0';
-										strcpy(_bufferRcv, bufferRcv);
-
-										if(checkKindMessage(_bufferRcv,0) == FINISHED){
-											printUserList(_users);		
-											waitKey();
-
-										}
-										strcpy(_bufferRcv, bufferRcv);
-									}
-
-								break;
-								//Entra na sala de conversa	
-								}case 2:{
-									printHeader("Sala de conversa");
-
-									isChatting = 1;
-									while(isChatting){
-										 fflush(stdin);
-										 while(!kbhit()){
-
-											bufferSend = generateMessage("", UPDATECHAT,0);
-											send(localSocket, bufferSend, MAXDATASIZE, 0);	
-
-											int i = 0;
-											_messages.size = i;
-
-											numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
-											bufferRcv[numBytes] = '\0';
-											strcpy(_bufferRcv, bufferRcv);
-
-											while(checkKindMessage(_bufferRcv,0) != FINISHED){
-												_messages.size = i+1;	
-												strcpy(_messages.content[i], checkMessage(bufferRcv));
-												i++;
-
-												numBytes = recv(localSocket, bufferRcv, MAXDATASIZE, 0);
-												bufferRcv[numBytes] = '\0';
-												strcpy(_bufferRcv, bufferRcv);
-
-												if(checkKindMessage(_bufferRcv,0) == FINISHED){
-													printMessageList(_messages);		
-												}
-
-												strcpy(_bufferRcv, bufferRcv);
-											}
-											 usleep(500000);
-										}
-
-										bufferKey = getchar();
-										if(bufferKey == '>'){	
-											printf(" [%s] - Voce: ", getHourMinutes());
-											scanf(" %[^\n]s", messageChat);
-											printf("\n");
-											
-											bufferSend = generateMessage(messageChat, MESSAGECHAT,0);
-											send(localSocket, bufferSend, MAXDATASIZE, 0);
-										}else if(bufferKey == 'v'){
-											isChatting = 0;
-										}else if(bufferKey == 'l'){
-											printHeader("Sala de conversa");
-										}else{
-											fflush(stdin);
-										}
-								}
-
-								break;	
-								//Solicita o fechamento de conexão ao servidor e finaliza o cliente
-								}case 3:{
-									bufferSend = generateMessage("",CLOSE,0);
-									send(localSocket, bufferSend, MAXDATASIZE, 0);	
-									printAlert("Tchau!");
-									
-									free(bufferSend);
-									free(bufferRcv);
-									free(_bufferRcv);
-									return 0;
-								break;
-								}
-							}
-						}while(opt2 != 3);
 					break;
-				//Mostra as instruções de uso ao cliente	
-				}case 2:{
-					printHowWorks();
-					waitKey();
-					break;
-					
-				}case 3:{
-					bufferSend = generateMessage("",CLOSE,0);
-					send(localSocket, bufferSend, MAXDATASIZE, 0);	
-					printAlert("Até mais ver, em francês, Au revoir");
-				
+					// Solicita o fechamento de conexão ao servidor e finaliza o cliente
+				}
+				case 3:
+				{
+					bufferSend = generateMessage("", CLOSE, 0);
+					send(localSocket, bufferSend, MAXDATASIZE, 0);
+					printAlert("Tchau!");
+
 					free(bufferSend);
 					free(bufferRcv);
 					free(_bufferRcv);
 					return 0;
-				}	
-			}
-		}while(opt != 3);	
-		
-		free(bufferSend);
-		free(bufferRcv);
-		free(_bufferRcv);
-				
-		return 0;
+					break;
+				}
+				}
+			} while (opt2 != 3);
+			break;
+			// Mostra as instruções de uso ao cliente
+		}
+		case 2:
+		{
+			printHowWorks();
+			waitKey();
+			break;
+		}
+		case 3:
+		{
+			bufferSend = generateMessage("", CLOSE, 0);
+			send(localSocket, bufferSend, MAXDATASIZE, 0);
+			printAlert("Até mais ver, em francês, Au revoir");
+
+			free(bufferSend);
+			free(bufferRcv);
+			free(_bufferRcv);
+			return 0;
+		}
+		}
+	} while (opt != 3);
+
+	free(bufferSend);
+	free(bufferRcv);
+	free(_bufferRcv);
+
+	return 0;
 }
--- header/interface.c
+++ header/interface.c
@@ -3,30 +3,34 @@
 #include <stdlib.h>
 
 #include "interface.h"
-#include "systime.h"	
+#include "systime.h"
 
-void printHeader(char * indentifier){
-	int i;	
+void printHeader(char *indentifier)
+{
+	int i;
 	int size = strlen(indentifier);
-	int padding = (78 - size)/2;
+	int padding = (78 - size) / 2;
 	system("clear");
-	printf("--------------------------------------------------------------------------------\n");	
-	
-	for(i=0;i<padding; i++){
+	printf("--------------------------------------------------------------------------------\n");
+
+	for (i = 0; i < padding; i++)
+	{
 		printf("#");
 	}
-	
+
 	printf(" %s ", indentifier);
-	
-	for(i=0;i<padding; i++){
-		printf("#");	
+
+	for (i = 0; i < padding; i++)
+	{
+		printf("#");
 	}
-	
+
 	printf("                                                         Último acesso: %s\n", getHourMinutesSeconds());
 	printf("--------------------------------------------------------------------------------\n");
 }
 
-void printMenuOptions(){
+void printMenuOptions()
+{
 	printf("\n");
 	printf("1 | Conectar-se\n");
 	printf("2 | Funcionalidades\n");
@@ -35,59 +39,68 @@ void printMenuOptions(){
 	printf("Escolha sua opção: ");
 }
 
-void printSecondMenuOptions(){
+void printSecondMenuOptions()
+{
 	printf("\n");
 	printf("1 | Usuários ativos\n");
-	printf("2 | Bate papo\n");
+	printf("2 | Chat\n");
 	printf("3 | Sair\n");
 	printf("\n");
 	printf("Escolha sua opção: ");
 }
 
-void printUserList(struct userList _users){
+void printUserList(struct userList _users)
+{
 	int i = 0;
-	printHeader("Usuários ativos");								
+	printHeader("Usuários ativos");
 	printf("Usuário número                  Usuário                  Atualizado às: %s\n", getHourMinutesSeconds());
-	
-	for(i=0;i<_users.size;i++){
+
+	for (i = 0; i < _users.size; i++)
+	{
 		printf("      %d                          %s\n", i, _users.name[i]);
 	}
-								
+
 	printf("\n");
 	printf("------------------------------- FIM DA LISTA -----------------------------------\n");
 }
 
-void printMessageList(struct messageList _messages){
+void printMessageList(struct messageList _messages)
+{
 	int i = 0;
-	
-	for(i=0;i<_messages.size;i++){
-		printf("> %s\n",_messages.content[i]);
+
+	for (i = 0; i < _messages.size; i++)
+	{
+		printf("> %s\n", _messages.content[i]);
 	}
 }
 
-void printAlert(char *message){
-	int i;	
+void printAlert(char *message)
+{
+	int i;
 	int size = strlen(message);
-	int padding = (76 - size)/2;
-	
+	int padding = (76 - size) / 2;
+
 	printf("\n");
 	printf("\n");
-	
-	for(i=0;i<padding; i++){
+
+	for (i = 0; i < padding; i++)
+	{
 		printf(" ");
-	}	
+	}
 
 	printf(">> %s <<", message);
-	
-	for(i=0;i<padding; i++){
-		printf(" ");	
+
+	for (i = 0; i < padding; i++)
+	{
+		printf(" ");
 	}
 	printf("\n");
 }
 
-void printHowWorks(){
+void printHowWorks()
+{
 	printHeader("Funcionalidades");
-	printAlert("O Bate-papo versão beta é simples e intuitivo!");	
+	printAlert("O Chat é simples e intuitivo!");
 	printf("\n");
 	printf("Siga os passos: \n");
 	printf("* Escolha a opçao conectar-se.\n");
@@ -99,36 +112,34 @@ void printHowWorks(){
 	printf("         *Insira a tecla 'l' para limpar a tela.\n");
 	printf("         *Menssagens com '@nome ' sao direcionadas apenas para o usuario 'nome'.\n");
 	printf("         		*Exemplo: '@joao Bom dia'\n");
-
 }
 
-void printAnimation(){
+void printAnimation()
+{
 	system("clear");
 	printf("\n\n\n\n\n\n");
-	printf("888888b.            888\n");
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("888   88b           888\n");
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("888  .88P           888\n"); 
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠿⠛⠛⠛⠛⢿⣿⣿⣿⣦⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("8888888K.   8888b.  888888  .d88b.         88888b.   8888b.  88888b.   .d88b.\n");  
+	printf("⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⡇⠀⠀⢸⣿⣿⣿⠀⢀⣤⣴⣶⣤⣤⡀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣶⣶⣤⣤⣄⠀⠀⠀⢠⣤⣼⣿⣿⣿⣤⣤⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("888   Y88b      88b 888    d8P  Y8b        888  88b      88b 888  88b d88  88b\n"); 
+	printf("⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠁⠀⠀⢸⣿⣿⣿⣴⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⢠⣾⣿⣿⡿⠿⠿⣿⣿⣿⣷⡄⠀⠸⠿⢿⣿⣿⣿⠿⠿⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("888    888 .d888888 888    88888888 888888 888  888 .d888888 888  888 888  888\n"); 
+	printf("⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡟⠁⠀⠀⠈⢻⣿⣿⣿⠀⠀⠀⠿⠿⠿⠏⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("888   d88P 888  888 Y88b.  Y8b.            888 d88P 888  888 888 d88P Y88..88P\n");
+	printf("⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠁⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⢀⣠⣤⣤⣶⣶⣾⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-	printf("8888888P    Y888888   Y888   Y8888         88888P    Y888888 88888P     Y88P \n"); 
+	printf("⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣶⣶⣶⡖⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⣴⣿⣿⡿⠛⠛⠉⠉⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-   	printf("                                           888               888\n");    
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣦⡀⠀⠀⠀⠀⣀⣼⣿⣿⣿⠃⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⢰⣿⣿⣿⡁⠀⠀⠀⢀⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-    printf("                                           888               888\n");
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⡿⠃⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠈⢿⣿⣿⣿⣶⣶⣶⣿⣿⣿⣿⣇⠀⠀⠀⠸⣿⣿⣿⣦⣤⠀⠀⠀⠀⠀⠀⠀\n");
 	usleep(100000);
-   	printf("                                           888               888\n");  
-	usleep(100000);	
-	printf("\n"); 		
-	printf("                                                                  Versão beta\n"); 		
+	printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⠀⠘⠛⠛⠛⠀⠀⠀⠀⠀⠘⠛⠛⠛⠀⠀⠀⠈⠙⠿⠿⠿⠟⠋⠁⠘⠛⠛⠛⠂⠀⠀⠀⠙⠻⠿⠿⠿⠀⠀⠀⠀⠀⠀⠀\n");
+	usleep(100000);
+	printf("\n");
 	sleep(1);
 }
-
